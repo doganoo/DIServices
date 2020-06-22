afe962c556286411ecc864dbc8de7fc25fd974a4
Dogan Ucar <dogan@dogan-ucar.de>
2020-06-22 20:09:40 +0200

    add datetime service

---
+++ src/public/DateTime/IDateTimeService.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Dogan Ucar, <dogan@dogan-ucar.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace doganoo\DI\DateTime;
+
+use DateTimeInterface;
+
+/**
+ * Interface IDateTimeService
+ *
+ * @package doganoo\DI\DateTime
+ * @author  Dogan Ucar <dogan@dogan-ucar.de>
+ */
+interface IDateTimeService {
+
+    /**
+     * Converts an unix timestamp to an instance of DateTimeInterface
+     *
+     * @param int $timestamp The unix timestamp to convert
+     *
+     * @return DateTimeInterface
+     */
+    public function toDateTime(int $timestamp): DateTimeInterface;
+
+    /**
+     * Converts an string formatted date/date time pattern to an instance of DateTimeInterface
+     * or null, if invalid
+     *
+     * @param string $format The formatted string
+     *
+     * @return DateTimeInterface|null
+     */
+    public function fromFormat(string $format): ?DateTimeInterface;
+
+}
---
+++ test/DateTime/DateTimeServiceTest.php
@@ -0,0 +1,109 @@
+<?php
+declare(strict_types=1);
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Dogan Ucar, <dogan@dogan-ucar.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace doganoo\DI\Test\HTTP\URL;
+
+use DateTimeInterface;
+use doganoo\DI\DateTime\IDateTimeService;
+use doganoo\DI\Test\Suite\TestCase;
+use doganoo\DIP\DateTime\DateTimeService;
+
+class DateTimeServiceTest extends TestCase {
+
+    /** @var IDateTimeService */
+    private $dateTimeService;
+
+    /**
+     * @param int $timestamp
+     *
+     * @dataProvider getTimestamps
+     */
+    public function testFromTimestamp(int $timestamp) {
+        $dateTime = $this->dateTimeService->toDateTime($timestamp);
+        $this->assertTrue($dateTime instanceof DateTimeInterface);
+        $this->assertTrue($dateTime->getTimestamp() === $timestamp);
+    }
+
+    /**
+     * @param string $format
+     * @param bool   $isNull
+     *
+     * @dataProvider getFormattedTimestamps
+     */
+    public function testFromFormat(string $format, bool $isNull) {
+        $dateTime = $this->dateTimeService->fromFormat($format);
+
+        if (true === $isNull) {
+            $this->assertNull($dateTime);
+        } else {
+            $this->assertTrue($dateTime instanceof DateTimeInterface);
+            $this->assertTrue($dateTime->getTimestamp() === strtotime($format));
+        }
+    }
+
+    public function getTimestamps(): array {
+        return [
+            [0]
+            , [1095379198]
+            , [1095379199]
+            , [1095379199]
+            , [1095379199]
+            , [1095379199]
+            , [1095379200]
+            , [1095379200]
+            , [1095379200]
+            , [1095379200]
+            , [1095379201]
+            , [1095379201]
+            , [-1]
+        ];
+    }
+
+    public function getFormattedTimestamps(): array {
+        return [
+            // Y-m-d H:i:s
+            ["1969-12-31 16:00:00", false]
+            , ["2004-09-16 16:59:58", false]
+            , ["2004-09-16 16:59:59", false]
+            , ["2004-09-16 16:59:59", false]
+            , ["2004-09-16 16:59:59", false]
+
+            // d.m.Y H:i:s
+            , ["16.09.2004 16:59:59", false]
+            , ["16.09.2004 16:59:59", false]
+            , ["16.09.2004 17:00:00", false]
+            , ["16.09.2004 17:00:00", false]
+            , ["16.09.2004 17:00:00", false]
+
+        ];
+    }
+
+    protected function setUp() {
+        parent::setUp();
+        $this->dateTimeService = new DateTimeService();
+    }
+
+}
