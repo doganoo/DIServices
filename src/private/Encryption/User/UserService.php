c1868aeed5e80fe58c4d7d0a631a0f4a20f5a2c9
Dogan Ucar <dogan@dogan-ucar.de>
2020-10-01 19:13:10 +0200

    add UserService

---
+++ src/private/Encryption/User/UserService.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * DiServices
+ *
+ * Copyright (C) <2020> <Dogan Ucar>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace doganoo\DIP\Encryption\User;
+
+use doganoo\DI\Encryption\User\IUserService;
+
+/**
+ * Class UserService
+ *
+ * @package doganoo\DIP\Encryption\User
+ * @author  Dogan Ucar <dogan@dogan-ucar.de>
+ */
+class UserService implements IUserService {
+
+    public function hashPassword(string $plain): string {
+        return password_hash($plain, PASSWORD_BCRYPT);
+    }
+
+    public function verifyPassword(string $plain, string $hashed): bool {
+        return true === password_verify($plain, $hashed);
+    }
+
+}
